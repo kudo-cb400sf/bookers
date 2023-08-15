04cd377a7b8e40e6bdb6328c3b00c28218167428
Takuma Kudo <138516260+kudo-cb400sf@users.noreply.github.com>
2023-08-15 10:51:32 +0000

    [finish] 3-15

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,7 +1,4 @@
 class BooksController < ApplicationController
-  def new
-    @book = Book.new
-  end
 
   def create
     @book = Book.new(book_params)
@@ -9,12 +6,13 @@ class BooksController < ApplicationController
       flash[:notice] = "Book was successfully created."
       redirect_to book_path(@book.id)
     else
-      flash.now[:notice] = "投稿に失敗しました。"
+      flash[:notice] = " errors prohibited this book from being saved:"
       render :new
     end
   end
 
   def index
+    @book = Book.new
     @books = Book.all
   end
 
@@ -24,23 +22,32 @@ class BooksController < ApplicationController
 
   def edit
     @book = Book.find(params[:id])
+    
   end
 
   def update
-    book = Book.find(params[:id])
-    book.update(book_params)
-    redirect_to book_path(book.id)
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      flash[:notice]="Book was successfully updated."
+    redirect_to book_path(@book.id)
+    else
+    flash[:notice] = " errors prohibited this book from being saved:"
+    render :edit
+    end 
   end
-
+  
   def destroy
     book = Book.find(params[:id])
     book.destroy
+    flash[:notice] = "Book was successfully destroyed."
     redirect_to '/books'
+    
   end
-
+  
   private
 
   def book_params
     params.require(:book).permit(:title, :body)
   end
+  
 end
